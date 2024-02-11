52f33d0f15848720a525a03dbb81bde2127f7d0b
bielrv <rosinol.gabriel@gmail.com>
2024-02-11 23:35:03 +0100

    New prompts

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,16 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'I have no defined career path; how do I move forward?',
+    message: `I have no defined career path; how do I move forward?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Provide 5 essential investment tips for me specifically',
+    message: 'Provide 5 essential investment tips for my specifically'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'How can I improve my discipline despite my difficulties',
+    message: `How can I improve my discipline despite my difficulties`
   }
 ]
 
@@ -24,14 +24,12 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to a16i Chatbot!
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
+          This a chatbot specifically trained as ato be your mentor and coach. 
+          Check for more info {' '}
+          <ExternalLink href="https://a16i.vercel.app/">Here</ExternalLink>
           .
         </p>
         <p className="leading-normal text-muted-foreground">
